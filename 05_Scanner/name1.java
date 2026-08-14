4e9d8763d3485a5d3c8df8cff8ae6c98ce44ea5e
Maadhavan S <maadhavan2081@gmail.com>
2026-08-14 12:33:07 +0530

    used next method to print string my name

---
+++ 05_Scanner/name1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class name1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String n = sc.next();
+        System.out.println("The name is : " + n);
+        
+    }
+}
