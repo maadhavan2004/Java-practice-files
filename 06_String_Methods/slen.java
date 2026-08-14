320946e28285a9ed77a1096251f7a6313390db30
Maadhavan S <maadhavan2081@gmail.com>
2026-08-14 14:31:52 +0530

    understood how to use the length method in string to get the length of the string

---
+++ 06_String_Methods/slen.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class slen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String s = sc.nextLine();
+        System.out.println("The length of the string is : " + s.length());
+    }
+}
