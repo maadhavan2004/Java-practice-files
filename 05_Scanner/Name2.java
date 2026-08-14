0613588ffdbaf9d51cca6d0c07d41ddf2d3bd6b4
Maadhavan S <maadhavan2081@gmail.com>
2026-08-14 12:35:31 +0530

    Used nextLine method to print my full name

---
+++ 05_Scanner/Name2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Name2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your full name: ");
+        String n = sc.nextLine();
+        System.out.println("The name is : " + n);
+        
+    }
+}
