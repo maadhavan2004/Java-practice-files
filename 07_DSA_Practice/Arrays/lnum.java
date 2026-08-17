6f47aecec39296a150df901f2e5415212d4a00e3
Maadhavan S <maadhavan2081@gmail.com>
2026-08-17 17:21:42 +0530

    in this code i learnt how to find the largest number in a array  1st step in DSA java

---
+++ 07_DSA_Practice/Arrays/lnum.java
@@ -0,0 +1,14 @@
+class lnum {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,50,40};
+        int largest = arr[0];
+
+        for(int i=0 ; i< arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+
+        System.out.println(" The Largest number is " + largest);
+    }
+}
